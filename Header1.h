44605229722043f1a13d95021142d70db70f8ea6
Amr mohamed <amr352361@gmail.com>
2026-02-24 02:26:27 +0200

    phase.2

--- Header1.h
+++ Header1.h
@@ -1,19 +1,40 @@
-//parser .h
+//parser .cpp
 #pragma once
-#include<vector>
-#include<string>
-#include<Client.h>
-#include<Employee.h>
-#include<Admin.h>
-using namespace std;
-class Parser {
-public:
-	static vector<string>split(string line);
-	
-	static Client parsetoclient(string line);
-	
-	static Employee parsetoEmployee(string line);
-	
-	static Admin parsetoAdmin(string line);
-	
-};
+#include"parser.h"
+#include<sstream>
+
+	 vector<string>Parser::split(string line) {
+		vector<string>result;
+		string temp;
+		stringstream ss(line);
+		while (getline(ss, temp, ',')) {
+			result.push_back(temp);
+		}
+		return result;
+	}
+	 Client Parser::parsetoclient(string line) {
+		vector<string>data = split(line);
+		string name = data[0];
+		string pass = data[1];
+		double balance = stod(data[2]);
+		Client c(name, pass, balance);
+		return c;
+	}
+	 Employee Parser::parsetoEmployee(string line) {
+		vector<string>data = split(line);
+		string name = data[0];
+		string pass = data[1];
+		double balance = stod(data[2]);
+		Employee e(name, pass, salary);
+		return e;
+	}
+	 Admin Parser::parsetoAdmin(string line) {
+		vector<string>data = split(line);
+		string name = data[0];
+		string pass = data[1];
+		double balance = stod(data[2]);
+		Admin a(name, pass, salary);
+		return a;
+	}
+
+}
