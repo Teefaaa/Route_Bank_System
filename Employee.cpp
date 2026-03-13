7c931a3580987b160016602db58bea835e76572e
Mahmoud <mahmoudmoustafaxx7@gmail.com>
2026-03-13 13:25:43 +0200

    before last inshallah

--- Employee.cpp
+++ Employee.cpp
@@ -8,133 +8,93 @@ double Employee::total_salaries = 0;
 void Employee::turn_on() { ; }
 
 Employee::Employee(string name, string password, double salary)
-    : Person(name, password)
-{
-    this->id = FilesHelper::getNextEmployeeId();
-    this->salary = salary;
-    this->total_salaries+=this->salary;
+    : Person(name, password) {
+  this->id = FilesHelper::getNextEmployeeId();
+  this->salary = salary;
+  this->total_salaries += this->salary;
 }
 
 Employee::Employee(int id, string name, string password, double salary)
-            : Person(name, password)
-{
-    this->id= id;
+    : Person(name, password) {
+  this->id = id;
+  this->salary = salary;
+  this->total_salaries += this->salary;
+}
+
+void Employee::set_salary(double salary) {
+  if (salary >= 5000) {
+    this->total_salaries -= this->salary;
     this->salary = salary;
+    this->total_salaries += this->salary;
+  } else {
+    cout << "Sorry! it should be greater than or equal 5000" << endl;
+  }
 }
 
-void Employee::set_salary(double salary)
-{
-    if (salary >= 5000)
-    {
-        this->total_salaries-=this->salary;
-        this->salary = salary;
-        this->total_salaries+=this->salary;
-    }
-    else
-    {
-        cout << "Sorry! it should be greater than or equal 5000" << endl;
-    }
+double Employee::get_salary() { return this->salary; }
+
+void Employee::activate_client(Client &c) {
+  c.setActive(true);
+  cout << "Client account activated successfully" << endl;
+  FilesHelper::saveAllClients();
 }
 
-double Employee::get_salary()
-{
-    return this->salary;
+void Employee::deactivate_client(Client &c) {
+  c.setActive(false);
+  cout << "Client account deactivated successfully" << endl;
+  FilesHelper::saveAllClients();
 }
 
-void Employee::activate_client(Client& c)
-{
-    c.setActive(true);
-    cout << "Client account activated successfully" << endl;
+void Employee::update_client_password(Client &recipient, string newPassword) {
+  if (!recipient.getActive())
+    return;
+  if (Validate::validate_password(newPassword)) {
+    recipient.set_password(newPassword);
+    cout << "Client password updated successfully" << endl;
     FilesHelper::saveAllClients();
+  } else {
+    cout << "Invalid password" << endl;
+  }
 }
 
-void Employee::deactivate_client(Client& c)
-{
-    c.setActive(false);
-    cout << "Client account deactivated successfully" << endl;
-    FilesHelper::saveAllClients();
+int Employee::count_clients_till(Client *c) { return c->get_id() - 2399999; }
+
+void Employee::view_client_balance(Client *c) {
+  cout << c->get_id() << " 's Balance: " << c->get_balance() << endl;
 }
 
-void Employee:: update_client_password(Client& recipient, string newPassword)
- {
-    if (!recipient.getActive())
-        return;
-    if (Validate::validate_password(newPassword))
-        {
-        recipient.set_password(newPassword);
-        cout << "Client password updated successfully" << endl;
-        FilesHelper::saveAllClients();
-		}
-		else
-        {
-        cout << "Invalid password" << endl;
-		}
-
-	}
-
-
-int	Employee:: count_clients_till(Client* c)
-{
-	return c->get_id() - 2399999;
+void Employee::display() {
+  cout << " \t\t  */\t\tEmployee Information\t\t\\* " << endl << endl;
+  Person::display();
+  cout << "Salary: " << this->salary << endl;
 }
-
-void Employee:: view_client_balance(Client* c)
-{
-    cout << c->get_id() << " 's Balance: " << c->get_balance() << endl;
-
-}
-
-
-
-void Employee::display()
-{
-    cout << " \t\t  */\t\tEmployee Information\t\t\\* " << endl << endl;
-    Person::display();
-    cout << "Salary: " << this->salary << endl;
-}
-
-
-
 
 /// =========== OTHERS ===================
 
-
-void Employee::addClient(Client& client)
-{
-    allClients.push_back(client);
-    FilesHelper::saveAllClients();
+void Employee::addClient(Client &client) {
+  allClients.push_back(client);
+  FilesHelper::saveAllClients();
 }
 
-Client* Employee::searchClient(int id)
-{
-    for (size_t i = 0; i < allClients.size(); i++)
-    {
-        if (allClients[i].get_id() == id)
-        {
-            return &allClients[i];
-        }
+Client *Employee::searchClient(int id) {
+  for (size_t i = 0; i < allClients.size(); i++) {
+    if (allClients[i].get_id() == id) {
+      return &allClients[i];
     }
-    return nullptr;
+  }
+  return nullptr;
 }
 
-void Employee::listClient()
-{
-    if (allClients.empty())
-    {
-        cout << "No clients found" << endl;
+void Employee::listClient() {
+  if (allClients.empty()) {
+    cout << "No clients found" << endl;
+  } else {
+    cout << " \t\t  */		Clients List		\\* " << endl
+         << endl;
+    for (size_t i = 0; i < allClients.size(); i++) {
+      cout << "Client no. " << i + 1 << endl;
+      allClients[i].display();
+      cout << endl;
     }
-    else
-    {
-        cout << " \t\t  */		Clients List		\\* " << endl << endl;
-        for (size_t i = 0; i < allClients.size(); i++)
-        {
-            cout << "Client no. " << i+1 << endl;
-            allClients[i].display();
-            cout << endl;
-        }
-    }
+  }
 }
-
-
-
-
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Route_Bank_System
+# Route Bank System
+
+**Cycle 25 – Fundamentals of Programming**
+
+---
+
+## Team Members
+
+| Name                    | Phone Number |
+| ----------------------- | ------------ |
+| Mostafa Aly Mostafa     | 1111107647   |
+| Mahmoud Samir Mahmoud   | 1128969594   |
+| Saeed Mohammed Saeed    | 1012814048   |
+| Kareem Mahmoud Kamal    | 1091752344   |
+| Amr Mohammed Elshabrawy | 1279823389   |
+
+---
+
+## Course Information
+
+* **Course:** Fundamentals of Programming
+* **Project:** Route Bank System
+* **Cycle:** 25
+
+---
+
+## Notes
+
